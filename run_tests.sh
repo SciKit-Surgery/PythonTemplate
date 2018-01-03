c16720f4789a0db75648d053d73b4a3a875e7e9a
Tom Doel <t.doel@ucl.ac.uk>
2018-01-03 18:09:04 +0000

    Updated tests to build project, build docs and run tests

--- run_tests.sh
+++ run_tests.sh
@@ -1,1 +1,12 @@
+base_dir=$(pwd)
+
 python -m unittest discover -s "tests" -p "*.py"
+
+mkdir -p build
+cookiecutter --output-dir build --no-input --overwrite-if-exists .
+
+cd build/MyNewProject/doc
+make html
+
+cd $base_dir/build/MyNewProject
+./run_tests.sh

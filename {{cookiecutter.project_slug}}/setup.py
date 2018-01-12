aeb8399ab0aeb0186ede5b2b13943eaff18f7633
Tom Doel <t.doel@ucl.ac.uk>
2018-01-12 17:38:37 +0000

    Issue #1: Exclude irrelevant items from API doc generation

--- {{cookiecutter.project_slug}}/setup.py
+++ {{cookiecutter.project_slug}}/setup.py
@@ -1,10 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='{{ cookiecutter.project_name }}',
-      version='0.1',
-      description='{{ cookiecutter.project_name }}',
-      url='{{ cookiecutter.project_url }}',
-      author='{{ cookiecutter.full_name }}',
-      author_email='YOUR-EMAIL@ucl.ac.uk',
-      license='{{ cookiecutter.open_source_license }}',
-      packages=['{{ cookiecutter.pkg_name }}'])
+setup(
+    name='{{ cookiecutter.project_name }}',
+    version='0.1',
+    description='{{ cookiecutter.project_name }}',
+    url='{{ cookiecutter.project_url }}',
+    author='{{ cookiecutter.full_name }}',
+    author_email='YOUR-EMAIL@ucl.ac.uk',
+    license='{{ cookiecutter.open_source_license }}',
+
+    packages=find_packages(
+        exclude=[
+            'doc',
+            'tests',
+        ]
+    )
+)

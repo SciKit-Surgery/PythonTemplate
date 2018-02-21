16f4856efaff98dc9fa5b07aae6b41f0211cc0d5
Tom Doel <t.doel@ucl.ac.uk>
2018-02-21 16:05:17 +0000

    Added hello_world entry point

---
+++ {{cookiecutter.project_slug}}/hello_world.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+import sys
+
+from {{cookiecutter.pkg_name}}.ui.hello_world import hello_world
+
+
+def main(args=None):
+    """Entry point for Helo World application"""
+
+    parser = argparse.ArgumentParser(description='Hello world demo')
+
+    parser.add_argument("-t", "--text", required=False, default=None,
+                        type=str
+                        help="Text to display")
+    version_string = get_version_string()
+    parser.add_argument(
+        "-v", "--version",
+        action='version',
+        version=version_string)
+
+    args = parser.parse_args(args)
+
+    hello_world(args.text)
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))

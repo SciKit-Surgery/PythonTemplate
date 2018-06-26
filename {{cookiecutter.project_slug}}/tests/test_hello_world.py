c7145ac627dc490bc2550103adfcda8dc130314c
Tom Doel <t.doel@ucl.ac.uk>
2018-06-26 15:05:07 +0100

    Space out cookiecutter variables for consistency

--- {{cookiecutter.project_slug}}/tests/test_hello_world.py
+++ {{cookiecutter.project_slug}}/tests/test_hello_world.py
@@ -4,9 +4,9 @@
 
 from unittest import TestCase
 
-from {{ cookiecutter.pkg_name }}.ui.{{cookiecutter.pkg_name}} import {{cookiecutter.pkg_name}}
+from {{ cookiecutter.pkg_name }}.ui.{{ cookiecutter.pkg_name }} import {{ cookiecutter.pkg_name }}
 
 
 class Test{{ cookiecutter.project_slug }}(TestCase):
-    def test_{{cookiecutter.pkg_name}}(self):
-        {{cookiecutter.pkg_name}}("Hello world")
+    def test_{{ cookiecutter.pkg_name }}(self):
+        {{ cookiecutter.pkg_name }}("Hello world")

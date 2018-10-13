eaf38e3967a9c640a977b0edd41bdc9a591fa3cb
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-10-13 16:57:13 +0100

    Issue #5: Provide option to remove top-level files, to ensure this project is just for modules.

---
+++ hooks/post_gen_project.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+"""Removes top-level __main__.py and {{ cookiecutter.project_slug }}.py if not needed."""
+
+SUCCESS = "\x1b[1;32m [SUCCESS]: "
+
+def remove_top_level_files():
+    file_names = [os.path.join("{{ cookiecutter.project_slug }}", "{{ cookiecutter.project_slug }}.py"),
+                  os.path.join("{{ cookiecutter.project_slug }}", "__main__.py")]
+    for file_name in file_names:
+        os.remove(file_name)
+
+def main():
+
+    if "{{ cookiecutter.project_runnable }}".lower() == "n":
+        remove_top_level_files()
+
+    print(SUCCESS + "Project initialized, keep up the good work!" + TERMINATOR)
+
+
+if __name__ == "__main__":
+    main()

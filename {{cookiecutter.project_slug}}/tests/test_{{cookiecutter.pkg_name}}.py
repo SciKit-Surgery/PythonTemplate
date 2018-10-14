a8487b8ffc55fd1dd52c5f5feaeb1563ce9f93fc
Matt Clarkson <m.clarkson@ucl.ac.uk>
2018-10-14 14:25:23 +0100

    Issue #6: Make first unit test not depend on run_demo.

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.pkg_name}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.pkg_name}}.py
@@ -2,11 +2,7 @@
 
 """{{ cookiecutter.project_name }} tests"""
 
-from unittest import TestCase
 
-from {{ cookiecutter.pkg_name }}.ui.{{ cookiecutter.pkg_name }}_demo import run_demo
-
-
-class Test{{ cookiecutter.project_slug }}(TestCase):
+class Test{{ cookiecutter.pkg_name }}(TestCase):
     def test_{{ cookiecutter.pkg_name }}(self):
-        run_demo(True, "Hello world")
+        pass

891ed48d593f2e0426172f8e7bff0291dda70e33
Stephen Thompson <s.thompson@ucl.ac.uk>
2018-11-06 13:04:51 +0000

    Issue #19 added a unit test using the TK Gui

--- {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.pkg_name}}.py
+++ {{cookiecutter.project_slug}}/tests/test_{{cookiecutter.pkg_name}}.py
@@ -4,8 +4,23 @@
 
 from {{ cookiecutter.pkg_name }}.ui.{{ cookiecutter.pkg_name }}_demo import run_demo
 
+import six
+import tkinte
+
 # Pytest style
 
 def test_using_pytest_{{ cookiecutter.pkg_name }}():
-    assert run_demo(True, "Hello World") == True
+    console = True
+    assert run_demo(console, "Hello World") == True
+
+def test_using_pytest_cookienewwithgitinit_withTK():
+    try:
+        console=False
+        assert run_demo(console, "Hello World") == True
+    except tkinter.TclError:
+        six.print_("Got TCL error, probably no DISPLAY set, that's OK.")
+        assert True
+    except:
+        six.print_("Got another error (not TCL), that's not OK.")
+        assert False
 

90be411369b8f50f24a78e2e1da3006dbcda265b
Stephen Thompson <s.thompson@ucl.ac.uk>
2018-11-13 14:54:24 +0000

    Revert "Issue #19 changed template tests to pytest"
    This reverts commit 7fecc1090ee882f6c26ef34e963e249028c13a70.

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,16 +5,19 @@ import os
 import subprocess
 import shutil
 import tempfile
+from unittest import TestCase
 
 import sys
 
 from cookiecutter.main import cookiecutter
 
-def test_template():
-    temp_folder = tempfile.mkdtemp()
-    cookiecutter('.', output_dir=temp_folder, no_input=True,
-        overwrite_if_exists=True)
-    return_code = subprocess.call([sys.executable, '-m', 'pytest'],
-        cwd=os.path.join(temp_folder, 'MyNewProject'))
-    shutil.rmtree(temp_folder)
-    assert return_code == True
+
+class TestTemplate(TestCase):
+    def test_template(self):
+        temp_folder = tempfile.mkdtemp()
+        cookiecutter('.', output_dir=temp_folder, no_input=True,
+                     overwrite_if_exists=True)
+        return_code = subprocess.call([sys.executable, '-m', 'unittest', 'discover'],
+                           cwd=os.path.join(temp_folder, 'MyNewProject'))
+        shutil.rmtree(temp_folder)
+        self.assertEqual(return_code, 0)

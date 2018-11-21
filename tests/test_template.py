4a36151c31d351dbbd38469bb00a8b33b22e778d
t <tdowrick@gmail.com>
2018-11-21 08:57:23 +0000

    #Issue 26: Change from unittest to pytest, update tox.ini test command

--- tests/test_template.py
+++ tests/test_template.py
@@ -5,19 +5,17 @@ import os
 import subprocess
 import shutil
 import tempfile
-from unittest import TestCase
 
 import sys
 
 from cookiecutter.main import cookiecutter
 
 
-class TestTemplate(TestCase):
-    def test_template(self):
-        temp_folder = tempfile.mkdtemp()
-        cookiecutter('.', output_dir=temp_folder, no_input=True,
-                     overwrite_if_exists=True)
-        return_code = subprocess.call([sys.executable, '-m', 'pytest'],
-                           cwd=os.path.join(temp_folder, 'MyNewProject'))
-        shutil.rmtree(temp_folder)
-        self.assertEqual(return_code, 0)
+def test_template():
+    temp_folder = tempfile.mkdtemp()
+    cookiecutter('.', output_dir=temp_folder, no_input=True,
+                    overwrite_if_exists=True)
+    return_code = subprocess.call([sys.executable, '-m', 'pytest'],
+                        cwd=os.path.join(temp_folder, 'MyNewProject'))
+    shutil.rmtree(temp_folder)
+    assert not return_code

1e07e4cd5fc75820baaf1fcb2fc55f00ddc8e2d0
Miguel Xochicale <m.xochicale@ucl.ac.uk>
2023-07-26 19:50:07 +0100

    optimise imports, reformats code tests/test_template (#69)

--- tests/test_template.py
+++ tests/test_template.py
@@ -2,20 +2,17 @@
 
 """Test the template"""
 import os
-import subprocess
 import shutil
-import tempfile
-
+import subprocess
 import sys
-
+import tempfile
 from cookiecutter.main import cookiecutter
 
 
-def test_template():
+def test_template_tests():
     temp_folder = tempfile.mkdtemp()
-    cookiecutter('.', output_dir=temp_folder, no_input=True,
-                    overwrite_if_exists=True)
-    return_code = subprocess.call([sys.executable, '-m', 'pytest'],
-                        cwd=os.path.join(temp_folder, 'MyNewProject'))
+    cookiecutter('.', output_dir=temp_folder, no_input=True, overwrite_if_exists=True)
+    return_code = subprocess.call([sys.executable, '-m', 'pytest'], cwd=os.path.join(temp_folder, 'MyNewProject'))
+    print(return_code)
     shutil.rmtree(temp_folder)
     assert not return_code
